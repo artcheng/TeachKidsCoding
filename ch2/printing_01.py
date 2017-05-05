a3b36b34f6aeebeeab68731aea7a5762c20561cf
artcheng <artcheng@gmail.com>
2017-05-05 16:25:23 -0400

    ch2

---
+++ ch2/printing_01.py
@@ -0,0 +1,28 @@
+print ('Hello World')
+
+#task 1: print your name
+
+
+#task 2: print your name 5 times
+
+
+#remove # to see how these print workds
+
+#print ('Yi Cheng '*5)
+#print ('Yi Cheng\n'*5)
+
+
+#then try something wrong, read the error message and try to fix it
+
+#print Yi Cheng
+#print YiCheng
+
+#The above two errors are different.
+#The first one said you have a syntax error, which means the computer doesn't understand your code
+#The second one saild YiCheng is not defined, which means the computer use YiCheng as a variable( we talk about it later),
+# but it doesn't what what value is for this variable.
+
+#Don't worry about the errors too much now. It is simply to know, when you write something wrong, the computer might tell you.
+
+
+
---
+++ ch2/variables_02.py
@@ -0,0 +1,34 @@
+#Variable likes a tag of some value
+
+name='Yi Cheng'
+print (name)
+
+#variable could tag to ANY type of value
+
+# string
+name='Yi Cheng'
+print (name)
+
+a='100'
+print a
+
+# number( integer )
+a=100
+print a
+
+#What is the difference between string and number?
+a='100'
+b='200'
+print 'string a+b is', a+b
+
+a=100
+b=200
+print 'number a+b is', a+b
+
+#Even for numbers, integer is different from decimal, so you have to chose the right type when you use it
+a=100 #integer
+print a/3
+
+a=100.0  #decimal number
+print a/3
+
