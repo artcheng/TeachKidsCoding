c6a7be226ebedb2d3e36cb0693978201de994c0b
artcheng <artcheng@gmail.com>
2017-05-10 13:18:24 -0400

    loop

--- ch2/loop_05.py
+++ ch2/loop_05.py
@@ -3,10 +3,46 @@
 for i in range(0, 10):
     print i
 
+print '-'*10
 
 a=10
 for i in range(0, a):
     print i,"'s square is ", i*i
 
+print '-'*10
 
-#task 1. let a=642, print all of a's factors
+#if you want to stop a loop in the middle, use break
+a=100
+for i in range(0,a):
+    print i
+    if i>10:
+        break
+
+print '-' * 10
+
+# understand the space in front each line.
+#1.
+a=5
+for i in range(0, 5):
+    x=i**2 #Do something inside the loop
+    print 'inside the for loop, i is ',i  #i is changing every time
+
+print '-' * 10
+#2
+for i in range(0, 5):
+    x=i**2 #Do something inside the loop
+print 'outside the for loop, i is ',i  #i is the last value when get out the loop
+
+
+
+
+#example 1. calculate 1+2+3+...+100
+n=101
+sum=0
+
+for i in range(1, n):
+    sum = sum + i
+print sum
+
+#task 1. calculate 15!=1*2*3*...*15
+
